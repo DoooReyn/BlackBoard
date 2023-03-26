3bf894afe605fea550c1e07f49c649cb304f3e8c
DoooReyn <jl88744653@gmail.com>
2023-03-26 23:23:25 +0800

    math

---
+++ runtime/sources/engine/core/util/math.ts
@@ -0,0 +1,12 @@
+export namespace math {
+    /**
+     * 比较两个数是否近似相等
+     * @param {number} a 数 a
+     * @param {number} b 数 b
+     * @param {number} precision 精度
+     * @returns {boolean}
+     */
+    export function equals( a : number, b : number, precision : number = 6 ) {
+        return Math.abs( a - b ) * Math.pow( 10, precision ) <= 1;
+    }
+}
